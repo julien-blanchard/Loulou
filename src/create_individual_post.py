40ea4455006ae2a0e87e322ced1e0b36274d51ce
Julien Blanchard <72625098+julien-blanchard@users.noreply.github.com>
2024-12-08 23:30:27 +0000

    Add files via upload

---
+++ src/create_individual_post.py
@@ -0,0 +1,15 @@
+def extractInfoFromMarkdown(data):
+    result = ""
+    return result
+
+def markdownToHtml(data):
+    result = ""
+    return result
+
+def htmlToBulma(data):
+    result = (
+        data
+        .replace("","")
+    )
+    return result
+
