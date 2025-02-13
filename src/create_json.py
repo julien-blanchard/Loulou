b12c2dd0375776c4e397f7b3bf3bb9401e67c798
Julien Blanchard <72625098+julien-blanchard@users.noreply.github.com>
2025-02-13 23:13:07 +0000

    Update create_json.py
    Added types

--- src/create_json.py
+++ src/create_json.py
@@ -16,14 +16,14 @@ def createPostsJSON(list_of_posts: List[str], path_to_json: str) -> Dict:
     for posts in list_of_posts:
         with open(posts, "r", encoding="utf8") as post_file:
             post: str = post_file.read()
-            post_config = post.split("-----")[1]
+            post_config: str = post.split("-----")[1]
             post_content: str = post.split("-----")[-1]
             post_title: str = os.path.split(posts)[-1]
             post: str = f'"{post_title}": {post_config}'
             temp_container.append(post)
     temp_container = ",".join(temp_container)
     temp_container = "{" + temp_container + "}"
-    unsorted_result = json.loads(temp_container)
+    unsorted_result: Dict = json.loads(temp_container)
     result = OrderedDict(reversed(sorted(unsorted_result.items())))
 
     json_file_name: str = os.path.join(path_to_json,"posts.json")
